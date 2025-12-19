7a25cfd0ad8b690b8390bfaab867ccf9600ab73d
Dayna Aamodt <daynaaamodt@Tors-MacBook-Air.local>
2025-12-19 14:08:15 -0800

    Configure wouter for GitHub Pages base path

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+  <Router base="/hermit-cove" hook={useHashLocation}>
     <App />
-  </HashRouter>
+  </Router>
 );
 
